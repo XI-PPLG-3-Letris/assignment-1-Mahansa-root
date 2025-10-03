63f8f96755533e1f2dd0e98c8304a64c429dff6e
Mahansa-root <lightningplay2009@gmail.com>
2025-10-03 13:32:13 +0700

    Add project

---
+++ project-1.js
@@ -0,0 +1,72 @@
+let a = 1;
+let b = 2;
+
+const pertambahan = () =>{
+    let c = a + b;
+    return c;
+}
+
+const pengurangan = () =>{
+    let c = a - b;
+    return c;
+}
+
+const pembagian = () =>{
+    let c = a / b;
+    return c;
+}
+
+const perkalian = () =>{
+    let c = a * b;
+    return c;
+}
+
+const perpangkatan = () =>{
+    let c = a ** b;
+    return c;
+}
+
+const modulus = () =>{
+    let c = a % b;
+    return c;
+}
+
+// console.log(`Hasil dari ${a} + ${b} = ${pertambahan()}`)
+// console.log(`Hasil dari ${a} - ${b} = ${pengurangan()}`)
+// console.log(`Hasil dari ${a} * ${b} = ${perkalian()}`)
+// console.log(`Hasil dari ${a} / ${b} = ${pembagian()}`)
+// console.log(`Hasil dari ${a} ^ ${b} = ${perpangkatan()}`)
+// console.log(`Hasil dari ${a} % ${b} = ${modulus()}`)
+
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
+console.log(`tambah(a,b)`)
+
+console.log(`log : Penjumlahan ${a} + ${b} = ${pertambahan()}`);
+
+console.log(`tambah(a,b)`)
+
+console.log(`log : Penjumlahan ${a} + ${b} = ${pertambahan()}`);
+
+
+console.log(`kurang(a,b)`)
+
+console.log(`log : Pengurangan ${a} - ${b} = ${pengurangan()}`);
+
+
+console.log(`kali(a,b)`)
+
+console.log(`log : Perkalian ${a} * ${b} = ${perkalian()}`);
+
+console.log(`bagi(a,b)`)
+
+console.log(`log : Pembagian ${a} / ${b} = ${pembagian()}`);
+
+console.log(`Modulus(a,b)`)
+
+console.log(`log : Modulus ${a} % ${b} = ${modulus()}`);
+
+console.log(`perpangkatan(a,b)`)
+
+console.log(`log : Perpangkatan ${a} ^ ${b} = ${perpangkatan()}`);
